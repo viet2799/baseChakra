9e15bcd74452a7d44e884eaf0928837486c46730
Nguyễn Đức Việt <43706629+viet2799@users.noreply.github.com>
2024-10-08 18:40:07 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
+import { IconButton } from "@chakra-ui/react";
 import "./App.css";
 import CommonButton from "./components/Common/Button";
+import { SearchIcon } from "@chakra-ui/icons";
 
 function App() {
-  const optionBtn = [
-    {
-      children: "123",
-      colorScheme: "red",
-    },
-    {
-      children: "123",
-      colorScheme: "green",
-    },
-  ];
+//   const optionBtn = [
+//     {
+//       children: "123",
+//       colorScheme: "red",
+//       size: "lg",
+//     },
+//     {
+//       children: "123",
+//       colorScheme: "green",
+//     },
+//   ];
   return (
     <>
-      <CommonButton isGroup optionBtn={optionBtn} gap={4} colorScheme="red">
-        aaaa
-      </CommonButton>
+      <CommonButton colorScheme="blue" aria-label="aaaaaaaa"  rounded={4} icon={<SearchIcon />} isIconBtn />
+
+      <IconButton colorScheme="blue" aria-label="aaaa" icon={<SearchIcon />} />
     </>
   );
 }
--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -5,21 +5,20 @@ import {
   IconButton,
   IconButtonProps,
 } from "@chakra-ui/react";
-import "./App.css";
 
-interface IButton extends ButtonProps {
+interface IButton extends Omit<ButtonProps,"aria-label">, IconButtonProps {
   isGroup?: boolean;
   children?: React.ReactNode | string;
   isIconBtn?: boolean;
   optionBtn?: IButton[];
   gap?: number;
-  SearchIcon: React.ReactElement;
 }
 
-interface IIconBtn extends IconButtonProps {
-  children?: React.ReactNode | string;
-  SearchIcon: React.ReactElement;
-}
+
+// const IconBtn = (props: IIconBtn) => {
+//   const { icon, descriptionIcon = "", ...restProp } = props;
+//   return <IconButton aria-label={descriptionIcon} icon={icon} {...restProp} />;
+// };
 
 const CommonButton = (props: IButton) => {
   const {
@@ -27,28 +26,25 @@ const CommonButton = (props: IButton) => {
     isGroup,
     optionBtn,
     gap = 2,
-    isIconBtn,
     size = "xs",
-    SearchIcon,
+    isIconBtn,
+    icon,
     ...restProps
   } = props;
 
-  
   if (isGroup) {
     return (
       <ButtonGroup gap={gap}>
         {optionBtn?.map((btn) => {
-          return (
-            <Button size={btn?.size} {...btn}>
-              {btn.children}
-            </Button>
-          );
+          return <Button {...btn}>{btn.children}</Button>;
         })}
       </ButtonGroup>
     );
   }
   if (isIconBtn) {
-    return <IconBtn />;
+    return (
+      <IconButton  aria-label="" icon={icon} {...restProps} />
+    );
   }
   return (
     <Button size={size} {...restProps}>
