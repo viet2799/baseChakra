f39fd30d5aee04b6606184c46c428743b12e57ce
Nguyễn Đức Việt <43706629+viet2799@users.noreply.github.com>
2024-11-18 09:17:45 +0700

    .

--- src/components/CustomeField/RadioField.tsx
+++ src/components/CustomeField/RadioField.tsx
@@ -1,67 +1,72 @@
-import { Radio, RadioGroup, RadioProps } from "@chakra-ui/react";
-import classNames from "classnames";
-import { get } from "lodash";
-import { ControllerRenderProps, UseFormStateReturn } from "react-hook-form";
-
-interface IOptionData {
-  label?: string;
-  valueRadio: string | number;
-}
-
-interface IRadioFieldProps extends RadioProps {
-  isGroup?: boolean;
-  children?: React.ReactNode;
-  label?: string;
-  field?: ControllerRenderProps<any, any>;
-  formState?: UseFormStateReturn<any>;
-  onChangeCustome?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  customeStyleError?: string;
-  isGroupRadio?: boolean;
-  optionData?: IOptionData[];
-}
-const RadioField = ({
-  field,
-  formState,
-  customeStyleError,
-  label,
-  isGroupRadio,
-  optionData,
-  ...restProps
-}: IRadioFieldProps) => {
-  const { name, value, onChange } = field || {};
-  const { touchedFields, errors, isSubmitted } = formState || {};
-  const isTouched = get(touchedFields, name!);
-  const errorMessage = get(errors, name!)?.message;
-
-  const renderError = () => {
-    if (isTouched || isSubmitted) {
+import {
+    Radio,
+    RadioGroup,
+    RadioProps,
+    RadioGroupProps,
+  } from "@chakra-ui/react";
+  import classNames from "classnames";
+  import { get } from "lodash";
+  import { ControllerRenderProps, UseFormStateReturn } from "react-hook-form";
+  
+  interface IOptionData {
+    label?: string;
+    valueRadio: string | number;
+    typeRadio?: RadioProps;
+  }
+  
+  interface IRadioFieldProps extends RadioGroupProps {
+    isGroup?: boolean;
+    label?: string;
+    field?: ControllerRenderProps<any, any>;
+    formState?: UseFormStateReturn<any>;
+    onChangeCustome?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    customeStyleError?: string;
+    isGroupRadio?: boolean;
+    optionData?: IOptionData[];
+  }
+  const GroupRadioField = ({
+    field,
+    formState,
+    customeStyleError,
+    optionData,
+    ...restProps
+  }: IRadioFieldProps) => {
+    const { name, value, onChange } = field || {};
+    const { touchedFields, errors, isSubmitted } = formState || {};
+    const isTouched = get(touchedFields, name!);
+    const errorMessage = get(errors, name!)?.message;
+    const renderError = () => {
+      if (isTouched || isSubmitted) {
+        return (
+          Boolean(errorMessage) && (
+            <div className={classNames("", customeStyleError)}>
+              {errorMessage?.toString()}
+            </div>
+          )
+        );
+      }
+    };
+  
+    const renderRadioField = (item: IOptionData) => {
+      const { valueRadio, label, typeRadio } = item;
+      const { ...restPropsRadio } = typeRadio || {};
+  
       return (
-        Boolean(errorMessage) && (
-          <div className={classNames("", customeStyleError)}>
-            {errorMessage?.toString()}
-          </div>
-        )
+        <Radio {...restPropsRadio} value={valueRadio}>
+          {label}
+        </Radio>
       );
-    }
-  };
-
-  const renderRadioField = (item: IOptionData) => {
-    const { valueRadio, label } = item;
+    };
+  
     return (
-      <Radio {...restProps} checked={value === valueRadio} onChange={onChange}>
-        {label}
-      </Radio>
+      <div>
+        <RadioGroup {...restProps} onChange={onChange}>
+          {optionData?.map((item: IOptionData) => renderRadioField(item))}
+        </RadioGroup>
+        {renderError()}
+      </div>
     );
   };
-
-  return (
-    <div>
-      <RadioGroup {...restProps}>
-        {optionData?.map((item: IOptionData) => renderRadioField(item))}
-      </RadioGroup>
-      {renderError()}
-    </div>
-  );
-};
-
-export default RadioField;
+  
+  export default GroupRadioField;
+  
